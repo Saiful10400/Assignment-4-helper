a5a5c2a13cc5a1dbd7d808a99fd50d8190fa8428
saiful10400 <saifulislam10400i5@gmail.com>
2024-09-11 10:14:08 +0600

    item added.

--- src/Booking.tsx
+++ src/Booking.tsx
@@ -20,30 +20,31 @@ const Booking = () => {
     setRoom(formData)
   };
   return (
-    <div>
-      {!isLoading && !isError&&
-        data.data.map((item, idx) => {
-          return (
-            <h1 key={idx}>
-              {(idx += 1)}. Room name:{" "}
-              <span className="font-bold text-lg text-green-600">
-                {item.name}
-              </span>
-            </h1>
-          );
-        })}
+//     <div>
+//       {!isLoading && !isError&&
+//         data.data.map((item, idx) => {
+//           return (
+//             <h1 key={idx}>
+//               {(idx += 1)}. Room name:{" "}
+//               <span className="font-bold text-lg text-green-600">
+//                 {item.name}
+//               </span>
+//             </h1>
+//           );
+//         })}
 
-      <div className="mt-5  w-max">
-        <form
-          onSubmit={submitHandle}
-          action="
-"
-        >
-          <input name="name" type="text" className="border border-black mr-5 py-4 px-2 text-lg font-semibold" />
-          <button className="btn btn-primary">Submit</button>
-        </form>
-      </div>
-    </div>
+//       <div className="mt-5  w-max">
+//         <form
+//           onSubmit={submitHandle}
+//           action="
+// "
+//         >
+//           <input name="name" type="text" className="border border-black mr-5 py-4 px-2 text-lg font-semibold" />
+//           <button className="btn btn-primary">Submit</button>
+//         </form>
+//       </div>
+//     </div>
+<h1>Saiful</h1>
   );
 };
 
